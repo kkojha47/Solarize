4d2585c174ddb164adf3fa6061a968e2c087e67e
Kojha <kojha@asu.edu>
2021-02-08 17:56:33 -0700

    added login & signup page

--- src/App.js
+++ src/App.js
@@ -1,92 +1,67 @@
 
 import React from "react";
 import "./App.css";
-import { Button, Card, Form } from 'react-bootstrap';
+import {Route, BrowserRouter as Router, Link} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Home from "./Components/Home"
+import Todo from "./Components/Todo"
+import About from "./Components/About"
+import Login from "./Components/Login"
+import Signup from "./Components/Signup"
 
-function Todo({ todo, index, markTodo, removeTodo }) {
-    return (
-        <div className="todo">
-            <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
-            <div>
-                <Button variant="outline-success" onClick={() => markTodo(index)}>✓</Button>{' '}
-                <Button variant="outline-danger" onClick={() => removeTodo(index)}>✕</Button>
-            </div>
-        </div>
-    );
-}
 
-function FormTodo({ addTodo }) {
-    const [value, setValue] = React.useState("");
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        if (!value) return;
-        addTodo(value);
-        setValue("");
-    };
+import desktop from "./media/desktop.jpg"
+import kanban from "./media/kanban.jpg"
+import postIts from "./media/postIts.jpg"
+import logo from "./media/logo.svg"
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group>
-                <Form.Label><b>Add Todo</b></Form.Label>
-                <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
-            </Form.Group>
-            <Button variant="primary mb-3" type="submit">
-                Submit
-            </Button>
-        </Form>
-    );
-}
 
 function App() {
-    const [todos, setTodos] = React.useState([
-        {
-            text: "This is a sampe todo",
-            isDone: false
-        }
-    ]);
-
-    const addTodo = text => {
-        const newTodos = [...todos, { text }];
-        setTodos(newTodos);
-    };
-
-    const markTodo = index => {
-        const newTodos = [...todos];
-        newTodos[index].isDone = true;
-        setTodos(newTodos);
-    };
-
-    const removeTodo = index => {
-        const newTodos = [...todos];
-        newTodos.splice(index, 1);
-        setTodos(newTodos);
-    };
-
     return (
-        <div className="app">
-            <div className="container">
-                <h1 className="text-center mb-4">Todo List</h1>
-                <FormTodo addTodo={addTodo} />
-                <div>
-                    {todos.map((todo, index) => (
-                        <Card>
-                            <Card.Body>
-                                <Todo
-                                    key={index}
-                                    index={index}
-                                    todo={todo}
-                                    markTodo={markTodo}
-                                    removeTodo={removeTodo}
-                                />
-                            </Card.Body>
-                        </Card>
-                    ))}
+        <Router>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <Link className="navbar-brand brand" to="/">
+                        <img src={logo} alt={"Logo"} width="50em" height="50em"/>
+                        solarize
+                    </Link>
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link disabled" to="/">Pricing</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link disabled" to="/">Resources</Link>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/signup">Try for free</Link>
+                        </li>
+                    </ul>
                 </div>
+            </nav>
+            <div className="app">
             </div>
-        </div>
+            <Route path="/" exact component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/todo" component={Todo}/>
+            <Route path="/login" component={Login}/>
+            <Route path="/signup" component={Signup}/>
+        </Router>
+
     );
 }
 
